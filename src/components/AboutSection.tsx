494694befb3ca1dc39443972a2b4dfff7a2652ce
theghost1980 <saturnob612@gmail.com>
2025-07-25 12:53:26 -0400

    upd: correcting gramatic

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -20,7 +20,7 @@ const musicians: Musician[] = [
     id: 0,
     name: 'Saturno Mangieri',
     role: 'Multi-instrumentista, Cantante',
-    bio: 'Apasionado cantante y percusionista que ha grabado producciones musicales en Venezuela y a nivel internacional. Musico de guataca con corazon!',
+    bio: 'Apasionado cantante y percusionista que ha grabado producciones musicales en Venezuela y a nivel internacional. Músico de guataca con corazón.',
     avatar: '/avatars/saturno.png',
     instruments: ['voice', 'guitar', 'percussion'],
   },
@@ -28,7 +28,7 @@ const musicians: Musician[] = [
     id: 1,
     name: 'Adrián Cedeño',
     role: 'Multi-instrumentista, Cantante y Arreglista',
-    bio: 'Músico desde muy temprana edad, que ha defendido, valorado y practicado el folklore venezolano. Pertenece al sistema de orquestas de Venezuela y su energía y pasión por la música, no tienen limites',
+    bio: 'Músico desde muy temprana edad, que ha defendido, valorado y practicado el folklore venezolano. Pertenece al sistema de orquestas de Venezuela y su energía y pasión por la música, no tienen límites',
     avatar: '/avatars/adrian.JPG',
     instruments: ['voice', 'cuatro', 'percussion'],
   },
@@ -36,7 +36,7 @@ const musicians: Musician[] = [
     id: 2,
     name: 'Hanuman Sánchez',
     role: 'Multi-instrumentista, Cantante y Percusionista Profesional',
-    bio: 'Hijo de padres músicos, egresado del sistema de orquestas de la Fundación Conservatorio "Vicente Emilio Sojo". Ha participado activamente en muchos escenarios y podemos decir que su corazon late al ritmo latino y tradicional.',
+    bio: 'Hijo de padres músicos, egresado del sistema de orquestas de la Fundación Conservatorio "Vicente Emilio Sojo". Ha participado activamente en muchos escenarios y podemos decir que su corazón late al ritmo latino y tradicional.',
     avatar: '/avatars/hanuman.jpeg',
     instruments: ['voice', 'percussion', 'cuatro'],
   },
@@ -60,7 +60,7 @@ const musicians: Musician[] = [
     id: 5,
     name: 'Rama Tona',
     role: 'Flauta Traversa y Cantante',
-    bio: 'Músico egresado del sistema nacional de orquestas de Venezuela. Cuenta con amplia trayectoria a nivel musical y proviene de una familia de músicos. Nos deleita con su amplio desempeño para tocar y cantar multiples generos y estilos musicales.',
+    bio: 'Músico egresado del Sistema Nacional de Orquestas de Venezuela. Cuenta con amplia trayectoria a nivel musical y proviene de una familia de músicos. Nos deleita con su amplio desempeño para tocar y cantar múltiples generos y estilos musicales.',
     avatar: '/avatars/rama.jpeg',
     instruments: ['flute','voice'],
   },
@@ -68,7 +68,7 @@ const musicians: Musician[] = [
     id: 6,
     name: 'Juan S. Reinoso',
     role: 'Flauta Dulce, percusión y Cantante',
-    bio: 'Jóven músico que pertenece al sistema de orquestas juveniles del Estado Lara en su sistema de iniciación musical Simón Bolívar, Venezuela. Juan proviene de una larga tradición de músicos y nos deleita con su flauta, acompañamientos de percusión y su voz.',
+    bio: 'Jóven músico que pertenece al sistema de orquestas juveniles del Estado Lara en su Sistema de Iniciación Musical Simón Bolívar, Venezuela. Juan proviene de una larga tradición de músicos y nos deleita con su flauta, acompañamientos de percusión y su voz.',
     avatar: '/avatars/juan.jpeg',
     instruments: ['flute','voice', 'percussion'],
   },
@@ -76,7 +76,7 @@ const musicians: Musician[] = [
     id: 7,
     name: 'Nadir Sánchez',
     role: 'Guitarra y Cantante',
-    bio: 'Músico de larga trayectoria musical, compositor y arreglista. Nadir ha dedicado su vida a cantar la música que nos conecta con lo divino, en todas sus formas y nombres. Ha participado en diferentes agrupaciones y ha grabado varios albumes de World Music.',
+    bio: 'Músico de larga trayectoria, compositor y arreglista. Nadir ha dedicado su vida a cantar la música que nos conecta con lo divino, en todas sus formas y nombres. Ha participado en diferentes agrupaciones y ha grabado varios albumes de World Music.',
     avatar: '/avatars/nadir.jpeg',
     instruments: ['guitar','voice'],
   },
@@ -127,13 +127,13 @@ export function AboutSection() {
             {/* Columna del texto */}
             <div className="md:w-3/5">
               <p className="text-base leading-relaxed text-gray-300 md:text-lg">
-                El ensamble Compass Venezuela, es una agrupación que nace con el proposito de representar a Venezuela en una presentacion cultural que tendra lugar en la India, especificamente en Muddenahalli con motivo a la celebracion del aniversario numero 100 de Sai Baba. Nosotros hemos estado en el camino musical desde hace mucho e incluso somos una familia. Nuestro grupo quiere llevar un compendio de generos musicales que van desde el Joropo, la Gaita Zuliana, el Golpe Tocuyano, hasta la Parranda y el afrovenezolano.
+                El ensamble Compass Venezuela, es una agrupación que nace con el propósito de representar a Venezuela en una presentación cultural que tendrá lugar en la India, específicamente en Muddenahalli con motivo a la celebración del aniversario número 100 de Sai Baba. Nosotros hemos estado en el camino musical desde hace mucho e incluso somos como una familia. Nuestro grupo quiere llevar un compendio de géneros musicales que van desde el Joropo, la Gaita Zuliana, el Golpe Tocuyano, hasta la Parranda y el afrovenezolano.
               </p>
               <p className="mt-6 text-lg font-semibold italic text-cyan-400">
                 Los Sueños y deseos hacia Dios, se hacen realidad
               </p>
               <p className="mt-4 text-base leading-relaxed text-gray-300 md:text-lg">
-                Siempre hemos utilizado la musica como la expresion mas pura de nuestro sentir y nuestra cultura. Dios nos ha regalado un viaje unico para ir a India y poder llevar una muestra de folklore. Este viaje y esta oportunidad son un milagro. El Simple hecho de que podamos llevar a los mas jovenes a cantar y tocar para miles de personas, da fe de que el amor y las buenas acciones siempre seran recompensados.
+                Siempre hemos utilizado la música como la expresión mas pura de nuestro sentir y nuestra cultura. Dios nos ha regalado un viaje único para ir a India y poder llevar una muestra de folklore. Este viaje y esta oportunidad son un milagro en sí mismo. El simple hecho de que podamos llevar a los más jóvenes a cantar y tocar para miles de personas, da fe de que el amor y las buenas acciones siempre seran recompensados.
               </p>
             </div>
           </div>
